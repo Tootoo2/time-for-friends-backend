f634180b915179f6bf407dc8a08a33759a4fcf09
Anton <antonstromkarl@gmail.com>
2019-09-11 15:47:45 +0200

    added cors to enable the server speaking to frontend, also made a quick getter to find all people

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const persons = require('./Person');
 const data = require('./data');
+
+app.use(cors());
 
 mongoose.connect(`mongodb://localhost/timefriends`, {
 	useNewUrlParser: true,
@@ -22,6 +25,13 @@ app.get('/', (req, res) => {
 	res.send('Hello World');
 });
 
+app.get('/api/people', (req, res) => {
+	persons
+		.find()
+		.then(item => res.json(item))
+		.catch(err => console.error(err));
+});
+
 const createData = () => {
 	db.dropDatabase();
 	persons.create(data);
@@ -29,7 +39,7 @@ const createData = () => {
 
 const startWebServer = () => {
 	createData();
-	app.listen(3000, () => {
-		console.log('App listening on port 3000');
+	app.listen(3001, () => {
+		console.log('App listening on port 3001');
 	});
 };
