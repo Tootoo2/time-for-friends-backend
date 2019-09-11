12daf8da7c6114ee99ad38fb085855135a50eaf8
Anton <antonstromkarl@gmail.com>
2019-09-11 15:01:50 +0200

    inserted mock data inside the project. Create the database with data everytime the server starts

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const persons = require('./Person');
+const data = require('./data');
 
 mongoose.connect(`mongodb://localhost/timefriends`, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
-
 
 const db = mongoose.connection;
 
@@ -15,16 +15,21 @@ db.on('error', () => console.log('Error connecting to db'));
 
 db.once('open', () => {
 	console.log('connected to db');
-  startWebServer();
+	startWebServer();
 });
 
 app.get('/', (req, res) => {
 	res.send('Hello World');
 });
 
-function startWebServer() {
-  db.dropDatabase();
+const createData = () => {
+	db.dropDatabase();
+	persons.create(data);
+};
+
+const startWebServer = () => {
+	createData();
 	app.listen(3000, () => {
-    console.log('App listening on port 3000');
+		console.log('App listening on port 3000');
 	});
-}
+};
