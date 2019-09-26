a2c1abc584b9770e95b8b3a8f353ecbdf8ecc38c
Anton <antonstromkarl@gmail.com>
2019-09-26 11:30:10 +0200

    added some backend validation to adding a new contact

--- src/Person.js
+++ src/Person.js
@@ -3,17 +3,32 @@ const Schema = mongoose.Schema;
 
 let personSchema = new Schema({
 	name: {
-		firstName: String,
-		lastName: String,
+		firstName: {
+			type: String,
+		//	match: /^[A-ZÅÄÖa-zåäö][A-ZÅÄÖa-zåäö.\s-]{1,25}$/,
+			required: true,
+		},
+		lastName: {
+			type: String,
+		//	match: /^[A-ZÅÄÖa-zåäö][A-ZÅÄÖa-zåäö.\s-]{1,25}$/,
+			required: true,
+		},
 	},
-	phoneNumber: String,
-	email: String,
+	phoneNumber: {
+		type: String,
+		match: /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]{5,20}$/,
+		required: true,
+	},
+	email: {
+		type: String,
+		match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+		required: true,
+	},
 	location: {
-		country: String,
-		city: String,
-		timeZone: String,
+		country: { type: String, required: true },
+		city: { type: String, required: true },
+		timeZone: { type: String, required: true },
 	},
 });
-
 
 module.exports = mongoose.model('Person', personSchema);
